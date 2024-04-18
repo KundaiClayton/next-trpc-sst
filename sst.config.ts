1b1d44029c1e753af31ab46fc1fbab158504c9c5
Kundai Clayton <kundaichats@gmail.com>
2024-04-18 23:31:16 +0200

    Update deployment workflow, remove unused migration files, and modify database provider in schema.prisma

--- sst.config.ts
+++ sst.config.ts
@@ -1,3 +1,4 @@
+import { env } from "./src/env";
 import { SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
 
@@ -10,7 +11,11 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const site = new NextjsSite(stack, "site", {
+        environment: {
+          DATABASE_URL: env.DATABASE_URL,
+        },
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
